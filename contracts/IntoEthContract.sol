1741febd451ec4c01877461fefe65ee373730f12
justsomeguy <thenaders3@gmail.com>
2020-10-03 20:46:51 -0700

    updated ERC721 contract

--- contracts/IntoEthContract.sol
+++ contracts/IntoEthContract.sol
@@ -1,17 +1,23 @@
 pragma solidity ^0.6.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
-import "@openzeppelin/contracts/drafts/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract IntoTheEther is ERC721Full is Ownable {
+contract IntoTheEther is Ownable, ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
+    address private poolAddress;
 
-    constructor() ERC721Full("Into the Ether", "ITE") public {
+    constructor(address _poolAddress) public ERC721("Into the Ether", "ITE") {
+        poolAddress = _poolAddress;
     }
 
-    function mintToPool(address poolAddress, string memory tokenURI) public onlyOwner returns (uint256) {
+    function mintEpisode(string memory tokenURI)
+        public
+        onlyOwner
+        returns (uint256)
+    {
         _tokenIds.increment();
 
         uint256 newItemId = _tokenIds.current();
