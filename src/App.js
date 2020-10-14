a6b0af73cbe6528e731da885dce75616264679ae
justsomeguy <thenaders3@gmail.com>
2020-10-13 17:21:13 -0700

    added desktop ui

--- src/App.js
+++ src/App.js
@@ -7,32 +7,45 @@ import ethHubLogo from "./images/ethHubLogo.png";
 import {
     initializeContracts,
     getERC20Balance,
+    getPrizePeriodRemaining,
+    getTotalTicketCount,
 } from "./utils/contractFunctions";
 import { addressShortener } from "./utils/helperFunctions";
 
 function App() {
     const [address, setAddress] = useState("");
     const [shortAddress, setShortAddress] = useState("");
-    const [daiBalance, setDaiBalance] = useState("");
-    const [ticketBalance, setTicketBalance] = useState("");
+    const [daiBalance, setDaiBalance] = useState("0");
+    const [ticketBalance, setTicketBalance] = useState("0");
+    const [totalSupply, setTotalSupply] = useState("0");
+    const [timeRemaining, setTimeRemaining] = useState("0");
+
     useEffect(() => {
         loadContractData();
     });
 
     const loadContractData = async () => {
+        if (window.ethereum.networkVersion !== "4")
+            return window.alert("Please change to Rinkeby");
         const addresses = await window.ethereum.enable();
-        const address = addresses[0];
-        setAddress(address);
-        const shortAd = addressShortener(address);
+        const userAddress = addresses[0];
+        const shortAd = addressShortener(userAddress);
+        await initializeContracts();
+        const newTimeRemaining = await getPrizePeriodRemaining();
+        loadErc20Data(userAddress);
+
+        setTimeRemaining(newTimeRemaining);
         setShortAddress(shortAd);
-        initializeContracts();
-        loadErc20Data(address);
+        setAddress(userAddress);
     };
 
     const loadErc20Data = async (userAddress) => {
         const ticketResult = await getERC20Balance("EHM", userAddress);
-        setTicketBalance(ticketResult);
         const daiResult = await getERC20Balance("DAI", userAddress);
+        const newTotalSupply = await getTotalTicketCount();
+
+        setTotalSupply(newTotalSupply);
+        setTicketBalance(ticketResult);
         setDaiBalance(daiResult);
     };
 
@@ -43,30 +56,41 @@ function App() {
                     <img src={logo} alt="drop-logo" />
                     <h4>Podcast Pools</h4>
                 </div>
-                <h6 className="shortened-address">0xA34...34ca3</h6>
+                <h6 className="shortened-address">{shortAddress}</h6>
             </div>
             <div className="App__dashboard">
                 <div className="top-info">
-                    <div className="text">
-                        <h2>Into the Ether</h2>
-                        <p className="description-sentence">
-                            A podcast focusing on all things Ethereum and DeFi.
-                        </p>
-                        <p className="description-paragraph">
-                            This podcast features in-depth discussions with
-                            prominent guests in the space hosted by Eric Conner
-                            as well as weekly news recaps featuring Anthony
-                            Sassano.
-                        </p>
-                        <img src={ethLogo} alt="ethLogo" />
-                        <div className="btn-container">
-                            <button className="blue-btn">Deposit</button>
-                            <button className="gray-btn">Withdraw</button>
+                    <div className="text--background-color">
+                        <div className="text">
+                            <h2>Into the Ether</h2>
+                            <p className="description-sentence">
+                                A podcast focusing on all things Ethereum and
+                                DeFi.
+                            </p>
+                            <p className="description-paragraph">
+                                This podcast features in-depth discussions with
+                                prominent guests in the space hosted by Eric
+                                Conner as well as weekly news recaps featuring
+                                Anthony Sassano.
+                            </p>
+                            <img
+                                className="eth-logo--mobile"
+                                src={ethLogo}
+                                alt="ethLogo"
+                            />
+                            <div className="btn-container">
+                                <button className="blue-btn">Deposit</button>
+                                <button className="gray-btn">Withdraw</button>
+                            </div>
+                        </div>
+                        <div className="eth-logo__container">
+                            <img
+                                className="eth-logo--desktop"
+                                src={ethLogo}
+                                alt="ethLogo"
+                            />
                         </div>
                     </div>
-                    {/* <div>
-                        <img src={ethLogo} alt="ethLogo" />
-                    </div> */}
                 </div>
                 <div className="middle-info">
                     <div className="middle-block__text">
@@ -76,25 +100,72 @@ function App() {
                                 🏆
                             </span>
                         </h3>
-                        <div>
+                        <div className="prizes">
                             <img
                                 className="ethhub-logo"
                                 src={ethHubLogo}
                                 alt="ethHubLogo"
                             />
-                            <h5>EthHub Weekly #133</h5>
-                            <p className="episode-info">
-                                Kucoin Hacked for $150mn, State of EIP-1559,
-                                eth2 Spardina launch, Optimism launches testnet,
-                                Blocknative's mempool explorer and exploring the
-                                Ethereum mempool.
-                            </p>
-                            <p>Date: Tue, 08 Sep 2020</p>
-                            <p>Episode Duration: 49:28</p>
+                            <div className="prizes__text">
+                                <h5>EthHub Weekly #133</h5>
+                                <p className="episode-info">
+                                    Kucoin Hacked for $150mn, State of EIP-1559,
+                                    eth2 Spardina launch, Optimism launches
+                                    testnet, Blocknative's mempool explorer and
+                                    exploring the Ethereum mempool.
+                                </p>
+                                <p className="attribute">
+                                    Date: Tue, 08 Sep 2020
+                                </p>
+                                <p className="attribute">
+                                    Episode Duration: 49:28
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className="bottom-info"></div>
+                <div className="bottom-info">
+                    <div className="main-text-container">
+                        <div className="sub-text-container">
+                            <h6>
+                                Total Tickets{" "}
+                                <span role="img" aria-label="ticket">
+                                    🎟
+                                </span>
+                            </h6>
+                            <p>{totalSupply} EHM</p>
+                        </div>
+                        <div className="sub-text-container">
+                            <h6>
+                                Your Ticket Balance{" "}
+                                <span role="img" aria-label="balance">
+                                    ⚖️
+                                </span>
+                            </h6>
+                            <p>{ticketBalance} EHM</p>
+                        </div>
+                    </div>
+                    <div className="main-text-container ">
+                        <div className="sub-text-container">
+                            <h6>
+                                Ending In{" "}
+                                <span role="img" aria-label="clock">
+                                    🕒
+                                </span>
+                            </h6>
+                            <p>{timeRemaining}</p>
+                        </div>
+                        <div className="sub-text-container">
+                            <h6>
+                                Your Dai Balance{" "}
+                                <span role="img" aria-label="money-wings">
+                                    💸
+                                </span>
+                            </h6>
+                            <p>{daiBalance} DAI</p>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div className="App__footer"></div>
         </div>
--- src/utils/contractFunctions.js
+++ src/utils/contractFunctions.js
@@ -1,10 +1,10 @@
-import ethers from 'ethers';
+import ethers from "ethers";
 
 // ABIs
-import PodPrizeStratAbi from './abis/PodPoolStratV2.json';
-import PodPrizePoolAbi from './abis/PrizePool.json';
-import IntoEthTokenAbi from './abis/IntoTheEther.json';
-import ERC20Abi from './abis/ERC20.json';
+import PodPrizeStratAbi from "./abis/PodPoolStratV2.json";
+import PodPrizePoolAbi from "./abis/PrizePool.json";
+import IntoEthTokenAbi from "./abis/IntoTheEther.json";
+import ERC20Abi from "./abis/ERC20.json";
 
 // Contracts
 let strategyContract;
@@ -14,57 +14,75 @@ let DaiToken;
 let TicketToken;
 
 // Contract Addresses (Rinkey);
-const PRIZE_STRAT_ADDRESS = '0xDf577189ad9659070D903d1a7A22d0Fb9E07f2f8';
-const PRIZE_POOL_ADDRESS = '0x65D16e7C4A7CB9D8Fa2D8Bbe55916FfB6Efe87fD';
-const INTO_ETH_TOKEN_ADDRESS = '0x6325764783626E233F5ff4f2D8D1F2bcCD9d6105';
-const DAI_TOKEN_ADDRESS = '0x5592ec0cfb4dbc12d3ab100b257153436a1f0fea';
-const TICKET_TOKEN_ADDRESS = '0x8bEa329655C9809355922Ac70fd4B0E51ce3FBd8';
+const PRIZE_STRAT_ADDRESS = "0xDf577189ad9659070D903d1a7A22d0Fb9E07f2f8";
+const PRIZE_POOL_ADDRESS = "0x65D16e7C4A7CB9D8Fa2D8Bbe55916FfB6Efe87fD";
+const INTO_ETH_TOKEN_ADDRESS = "0x6325764783626E233F5ff4f2D8D1F2bcCD9d6105";
+const DAI_TOKEN_ADDRESS = "0x5592ec0cfb4dbc12d3ab100b257153436a1f0fea";
+const TICKET_TOKEN_ADDRESS = "0x8bEa329655C9809355922Ac70fd4B0E51ce3FBd8";
 
-const REFERRER_ADDRESS = '0xec8b449D4cedcc1BF688EAA40B7dFafd6d2D00DC'
+const REFERRER_ADDRESS = "0xec8b449D4cedcc1BF688EAA40B7dFafd6d2D00DC";
 
 export const initializeContracts = () => {
     try {
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         const signer = provider.getSigner();
-        strategyContract = new ethers.Contract(PRIZE_STRAT_ADDRESS, PodPrizeStratAbi.abi, signer);
-        poolContract = new ethers.Contract(PRIZE_POOL_ADDRESS, PodPrizePoolAbi.abi, signer);
-        IntoEthToken = new ethers.Contract(INTO_ETH_TOKEN_ADDRESS, IntoEthTokenAbi.abi, signer);
+        strategyContract = new ethers.Contract(
+            PRIZE_STRAT_ADDRESS,
+            PodPrizeStratAbi.abi,
+            signer
+        );
+        poolContract = new ethers.Contract(
+            PRIZE_POOL_ADDRESS,
+            PodPrizePoolAbi.abi,
+            signer
+        );
+        IntoEthToken = new ethers.Contract(
+            INTO_ETH_TOKEN_ADDRESS,
+            IntoEthTokenAbi.abi,
+            signer
+        );
         DaiToken = new ethers.Contract(DAI_TOKEN_ADDRESS, ERC20Abi.abi, signer);
-        TicketToken = new ethers.Contract(TICKET_TOKEN_ADDRESS, ERC20Abi.abi, signer);
+        TicketToken = new ethers.Contract(
+            TICKET_TOKEN_ADDRESS,
+            ERC20Abi.abi,
+            signer
+        );
     } catch (error) {
         throw new Error(`No injected ethereum provider: ${error}`);
     }
-}
+};
 
 export const getTotalTicketCount = async () => {
     try {
-        const result = TicketToken.totalSupply();
+        const result = await TicketToken.totalSupply();
         return ethers.utils.formatUnits(result, 18);
     } catch (error) {
         console.log(error);
-        return 'Total Ticket Error'
+        return "Total Ticket Error";
     }
-}
+};
 
 export const getPrizePeriodRemaining = async () => {
     try {
         const totalTimeSeconds = await strategyContract.prizePeriodRemainingSeconds();
-        const days = Math.floor(totalTimeSeconds / (3600*24));
-        const hours = Math.floor((totalTimeSeconds/3600)-(days*24));
-        const minutes = Math.floor((totalTimeSeconds/60) - (days*24*60 + hours*60));
-        return`${days}D ${hours}H ${minutes}M`;
+        const days = Math.floor(totalTimeSeconds / (3600 * 24));
+        const hours = Math.floor(totalTimeSeconds / 3600 - days * 24);
+        const minutes = Math.floor(
+            totalTimeSeconds / 60 - (days * 24 * 60 + hours * 60)
+        );
+        return `${days}D ${hours}H ${minutes}M`;
     } catch (error) {
         console.log(error);
-        return 'Time Remaining Error'
+        return "Time Remaining Error";
     }
-}
+};
 
 export const getERC20Balance = async (type, userAddress) => {
     let tokenContract;
-    if(type === undefined || userAddress === undefined){
-        console.log('Invalid type or address provided');
+    if (type === undefined || userAddress === undefined) {
+        console.log("Invalid type or address provided");
     }
-    type === 'DAI' ? tokenContract = DaiToken : tokenContract = TicketToken;
+    type === "DAI" ? (tokenContract = DaiToken) : (tokenContract = TicketToken);
     try {
         const weiBalance = await tokenContract.balanceOf(userAddress);
         const formattedBal = ethers.utils.formatUnits(weiBalance, 18);
@@ -72,47 +90,49 @@ export const getERC20Balance = async (type, userAddress) => {
         return roundedBal;
     } catch (error) {
         console.log(error);
-        return `Balance Error for ${type}`
+        return `Balance Error for ${type}`;
     }
-}
+};
 
 export const getCurrentNftPrizes = async () => {
     const ipfsHashArray = [];
     try {
-        const idArray = await strategyContract.getAwardErc721TokenIds(INTO_ETH_TOKEN_ADDRESS);
-        idArray.forEach( async NftId => {
+        const idArray = await strategyContract.getAwardErc721TokenIds(
+            INTO_ETH_TOKEN_ADDRESS
+        );
+        idArray.forEach(async (NftId) => {
             let ipfsHash = await IntoEthToken.tokenURI(NftId);
             ipfsHash = ipfsHash.slice(12); // remove the url from the hash
             ipfsHashArray.push(ipfsHash);
         });
-    return ipfsHashArray
+        return ipfsHashArray;
     } catch (error) {
         console.log(error);
     }
-}
+};
 
 export const depositDaiToPool = async (userAddress, value) => {
     try {
         const weiValue = ethers.utils.parseUnits(value, 18);
         const depositTx = await poolContract.depositTo(
-            userAddress, 
-            weiValue, 
-            TICKET_TOKEN_ADDRESS, 
+            userAddress,
+            weiValue,
+            TICKET_TOKEN_ADDRESS,
             REFERRER_ADDRESS
         );
-    await depositTx.wait();
-    return depositTx.hash;
+        await depositTx.wait();
+        return depositTx.hash;
     } catch (error) {
         console.log(error);
-        window.alert('There was an error depositing the Dai');
+        window.alert("There was an error depositing the Dai");
     }
-}
+};
 
 export const withdrawDaiFromPool = async (userAddress, value) => {
     try {
         const weiValue = ethers.utils.parseUnits(value, 18);
         //Max fee that the user is willing to pay if withdraw early
-        const maxFee = weiValue * 0.1; 
+        const maxFee = weiValue * 0.1;
         const withdrawTx = poolContract.withdrawInstantlyFrom(
             userAddress,
             weiValue,
@@ -123,6 +143,6 @@ export const withdrawDaiFromPool = async (userAddress, value) => {
         return withdrawTx.hash;
     } catch (error) {
         console.log(error);
-        window.alert('There was an error withdrawing the Dai');
+        window.alert("There was an error withdrawing the Dai");
     }
-}
+};
