99088c43694e39dd06ceb9e1daceccdc6770fa07
justsomeguy <thenaders3@gmail.com>
2020-10-20 09:44:15 -0700

    Updates

---
+++ precache-manifest.cb605d20ba233e24a765d721b85f8e06.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a563d2c9a828d3dfe009b54848e72faa",
+    "url": "/podcast-pools/index.html"
+  },
+  {
+    "revision": "c4b9323056776448e51e",
+    "url": "/podcast-pools/static/css/main.e80f0f90.chunk.css"
+  },
+  {
+    "revision": "f0c3fd40e741c83643e3",
+    "url": "/podcast-pools/static/js/2.1d3642e6.chunk.js"
+  },
+  {
+    "revision": "c12a39412f3a7839d6e01d52528c45d6",
+    "url": "/podcast-pools/static/js/2.1d3642e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4b9323056776448e51e",
+    "url": "/podcast-pools/static/js/main.5620f2c8.chunk.js"
+  },
+  {
+    "revision": "dcab6aee345c770e708e",
+    "url": "/podcast-pools/static/js/runtime-main.c548508d.js"
+  },
+  {
+    "revision": "68ae8a6c4f6118f953847771691daf20",
+    "url": "/podcast-pools/static/media/approve-contract.68ae8a6c.svg"
+  },
+  {
+    "revision": "38805e380487e9839aef39782bb907b7",
+    "url": "/podcast-pools/static/media/check-mark.38805e38.svg"
+  },
+  {
+    "revision": "83ca0030dbcdc07ab600421cf4866034",
+    "url": "/podcast-pools/static/media/clock.83ca0030.svg"
+  },
+  {
+    "revision": "6387d0c23b668f766b575459291c8bf3",
+    "url": "/podcast-pools/static/media/droplet-logo.6387d0c2.svg"
+  },
+  {
+    "revision": "c7be19d86c3e1b511822dcadcfb8052f",
+    "url": "/podcast-pools/static/media/eth-diamond-rainbow.c7be19d8.png"
+  },
+  {
+    "revision": "283623abc409a287a5498e89a4b744a6",
+    "url": "/podcast-pools/static/media/ethHubLogo.283623ab.png"
+  },
+  {
+    "revision": "7be246d45cf0b8f3d9db2ce66e3c26b2",
+    "url": "/podcast-pools/static/media/x.7be246d4.svg"
+  }
+]);
